faa717639ad883533d008f0b3f4af17f5b2fcc08
Paras Bagri <parasba85@gmail.com>
2023-11-30 16:32:15 +0530

    Day_1_four_commit

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className='flex justify-center items-center text-white text-3xl h-full'>
+      Welcome to StudyNotion!! Hii This is Paras Bagri and i'm Your Instructor !!
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = ({isLoggedIn}) => {
+  return (
+    <div className='flex justify-center items-center text-white text-3xl h-full'>
+    Namaskaara!! This is Paras Bagri,  <br/> You Can Create Your Own Account <br/> <br/> <mark>Login/sin Up</mark> 
+    </div>
+  )
+}
+
+export default Home
